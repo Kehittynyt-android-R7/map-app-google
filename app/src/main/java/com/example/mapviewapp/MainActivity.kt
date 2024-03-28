5eaf6adfca7f34c19c67678e7a190b1d1eed67d9
Santeri <santeri.hyry@gmail.com>
2024-03-28 12:54:09 +0200

    Add compose dependencies

--- app/src/main/java/com/example/mapviewapp/MainActivity.kt
+++ app/src/main/java/com/example/mapviewapp/MainActivity.kt
@@ -5,6 +5,15 @@ import android.os.Bundle
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     //Muuttuja Google maps kartan säilyttämiseen
